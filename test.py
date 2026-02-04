2d3c1139bd4d87db066e4f9922bbd568377a74fb
jpd6423 <jpd6423@psu.edu>
2026-02-04 15:15:29 -0500

    Add unit test
    Unit test to verify addBook function working for normal cases.

--- test.py
+++ test.py
@@ -1,3 +1,28 @@
 """
 Author: Steve
 """
+
+import unittest
+from unittest.mock import patch
+from io import StringIO
+
+# Import our user-made functions for testing
+from personal_library import ( 
+    addBook,
+    removeBook,
+    listBooks,
+    searchBooks
+)
+
+# Test class
+class TestPersonalLibrary(unittest.TestCase):
+    # Test case for addBook function
+    def test_addBook_normalCase(self):
+        library = []
+        with patch("builtins.input", return_value='Dune'): # Pass a value to the system input
+            addBook(library)
+        self.assertEqual(library, ['Dune'])
+
+# Call the test class main method
+if __name__ == "__main__":
+    unittest.main()
