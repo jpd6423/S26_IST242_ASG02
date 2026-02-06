1b8529fcf541277a18cbab6fe0790d39db3bbf2a
time233b <fsjaylo@gmail.com>
2026-02-06 09:29:53 -0500

    Add files via upload
    testing for library

--- test.py
+++ test.py
@@ -1,36 +1,53 @@
 """
-Author: Steve
-"""
-
+Testing library
+""" 
 import unittest
 from unittest.mock import patch
 from io import StringIO
-
-# Import our user-made functions for testing
-from personal_library import ( 
-    addBook,
-    removeBook,
-    listBooks,
-    searchBooks
+#import our user made function for testing
+from personal_library import(
+    add_book,
+    remove_book,
+    list_book,
+    search_book
 )
-
-# Test class
-class TestPersonalLibrary(unittest.TestCase):
-    # Test case for addBook function
-    def test_addBook_normalCase(self):
-        library = []
-        with patch("builtins.input", return_value='Dune'): # Pass a value to the system input
-            addBook(library)
-        self.assertEqual(library, ['Dune'])
-
-    def test_listBook_listsMoreThanOneBook(self):
-        library = ['Dune',"Dune Messiah"]
-        with patch("sys.stdout", new=StringIO()) as out:
-            listBooks(library)
+#test class 
+class TestPersonallibrary(unittest.TestCase):
+    #test case for add book function
+    def test_add_book_normal_case(self):
+        library =[]
+        with patch("builtins.input",return_value ="Dune"):
+           add_book(library)
+        
+        self.assertEqual(library, ["Dune"])
+    #test case for multiple books
+    def test_list_more_than_one_book(self):
+        library= ["Dune","Dune Messiah"]
+        with patch("sys.stdout",new=StringIO()) as out:
+            list_book(library)
             output = out.getvalue()
-        self.assertIn('Dune', output)
+        self.assertIn("Dune", output)
         self.assertIn("Dune Messiah", output)
+    #test remove book
+    def test_remove_book(self):
+        library =["book"]
+        with patch("builtins.input", return_value="book"):
+            remove_book(library)
+        self.assertEqual(library,[])
+    #test search function
+    def test_search_book(self):
+        library =["book"]
+        with patch("builtins.input", return_value="book"):
+            with patch("sys.stdout", new=StringIO()) as out:
+                search_book(library)
+            output = out.getvalue()
+        self.assertIn("book",output)
 
-# Call the test class main method
+      
+
+    
+
+#call the test class main method
+
 if __name__ == "__main__":
     unittest.main()
